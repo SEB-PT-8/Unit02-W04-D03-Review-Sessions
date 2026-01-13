777d5530ab20dcc2140319bfa0301d9f49411342
omarakamal <omaraakamal@gmail.com>
2026-01-13 20:36:11 +0300

    update

--- controllers/hotels.routes.js
+++ controllers/hotels.routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router()
-
+const Hotel = require('../models/Hotel')
 router.get('/',(req,res)=>{
     res.render('all-hotels.ejs') // do not add /all-hotels.ejs
 })
 
 router.get('/new',(req,res)=>{
     res.render('create-hotel.ejs')
+})
+
+router.post('/',async (req,res)=>{
+    try{
+    const createdHotel = await Hotel.create(req.body)
+    res.redirect('/hotels')
+    }
+    catch(err){
+        console.log(err)
+    }
+  
 })
 module.exports = router
