fb95b5bf9c6bc06893824931729aa37297afa383
omarakamal <omaraakamal@gmail.com>
2026-01-13 20:03:51 +0300

    update

--- controllers/hotels.routes.js
+++ controllers/hotels.routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 
 router.get('/',(req,res)=>{
-    res.render('all-hotels.ejs')
+    res.render('all-hotels.ejs') // do not add /all-hotels.ejs
+})
+
+router.get('/new',(req,res)=>{
+    res.render('create-hotel.ejs')
 })
 module.exports = router
