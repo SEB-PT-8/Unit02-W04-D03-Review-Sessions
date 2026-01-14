c95766ad7c574e7e2fec69592cf57babb2facd78
omarakamal <omaraakamal@gmail.com>
2026-01-14 18:01:24 +0300

    update

--- controllers/hotels.routes.js
+++ controllers/hotels.routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const Hotel = require('../models/Hotel')
+
+
 router.get('/',async (req,res)=>{
     const allHotels = await Hotel.find() // gets all the hotels
     res.render('all-hotels.ejs',{allHotels:allHotels}) // do not add /all-hotels.ejs
